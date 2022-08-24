f0a96bd2f792286fced10bd589553246c132c4db
Ольга Воронюк <helga-61rus@yandex.ru>
2022-08-24 13:36:57 +0300

    поправила импорты

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
