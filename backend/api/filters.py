074f9939e64aad1449d12b4063e1914772972fc9
Ольга Воронюк <helga-61rus@yandex.ru>
2022-08-24 13:56:20 +0300

    фильтры

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe
 
 User = get_user_model()
 
 
-class NameSearchFilter(filters.SearchFilter):
+class NameSearchFilter(SearchFilter):
     search_param = 'name'
 
 
